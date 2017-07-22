5be56611070a8d6aabbd46c21d8b6bfbf9f60f0b
test <test>
2017-07-22 12:54:04 +0400

    .nv

--- config/database.php
+++ config/database.php
@@ -65,19 +65,19 @@ return [
             'strict' => true,
             'engine' => null,
         ],
-
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => parse_url(getenv("DATABASE_URL"))["host"],
-            'port' => env('DB_PORT', '5432'),
-            'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
-            'username' => parse_url(getenv("DATABASE_URL"))["user"],
-            'password' => parse_url(getenv("DATABASE_URL"))["pass"],
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'prefer',
-        ],
+        
+		'pgsql' => [	
+			'driver' => 'pgsql',		   
+			'host' => env('DB_HOST', '127.0.0.1'),		 
+			'port' => env('DB_PORT', '5432'),		         
+			'database' => env('DB_DATABASE', 'forge'),	
+			'username' => env('DB_USERNAME', 'forge'),	
+			'password' => env('DB_PASSWORD', ''),		 
+			'charset' => 'utf8',		              
+			'prefix' => '',		             
+			'schema' => 'public',
+			'sslmode' => 'prefer',
+		]
 
     ],
 
